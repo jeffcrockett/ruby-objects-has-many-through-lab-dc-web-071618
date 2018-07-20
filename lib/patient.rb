dd4d0a86a31f65d1d168f3f8ec15decfa541d168
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-20 12:34:44 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,6 +15,6 @@ class Patient
   end
 
   def new_appointment(date, doctor)
-    appointment = Appointment.new(date, doctor, self)
+    appointment = Appointment.new(date, self, doctor)
   end
 end
